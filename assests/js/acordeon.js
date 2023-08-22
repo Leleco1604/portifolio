6a371268ed39393d41b35ed42f0f051e12e08a74
Leleco1604 <leonardogbarbosamelo2003@gmail.com>
2023-08-21 21:27:50 -0300

    adicionando animações no acordeon

---
+++ assests/js/acordeon.js
@@ -0,0 +1,14 @@
+const acordeoTriggers = document.querySelectorAll('.acordeon . trigger');
+
+acordeoTriggers.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+        const acordeon = trigger.parentElement
+        const isOpen = acordeon.classList.contains('open')
+
+        if (isOpen){
+            acordeon.classList.remove('open')
+        } else{
+            acordeon.classList.add('open')
+        }
+    })
+})
