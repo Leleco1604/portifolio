e52871ec74124566dd5b5567e20efb6873e4b074
Leleco1604 <leonardogbarbosamelo2003@gmail.com>
2023-08-23 17:20:40 -0300

    finalizando o Projeto

--- assests/js/acordeon.js
+++ assests/js/acordeon.js
@@ -4,11 +4,14 @@ acordeonTriggers.forEach((trigger) => {
     trigger.addEventListener('click', (e) => {
         const acordeon = trigger.parentElement;
         const isOpen = acordeon.classList.contains('open');
+        const arrowIcon = trigger.querySelector('.arrow-icon'); 
 
         if (isOpen) {
             acordeon.classList.remove('open');
+            arrowIcon.style.transform = 'rotate(0deg)';  
         } else {
             acordeon.classList.add('open');
+            arrowIcon.style.transform = 'rotate(180deg)';
         }
     });
 });
